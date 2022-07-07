14f766f08ca11cac544bda318ceb3964d47be5f5
swastikjain77 <59052688+swastikjain77@users.noreply.github.com>
2022-07-07 15:09:36 +0530

    Add files via upload

---
+++ React Digital Bussines Web page/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About(){
+    return(
+        <div className="about_div">
+            <p className="small_title" >About</p>
+            <p className="content">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        </div>
+    )
+}
---
+++ React Digital Bussines Web page/components/Like.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Like(){
+    return(
+          <div className="about_div">
+            <p className="small_title" >Interests</p>
+            <p className="content">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}
